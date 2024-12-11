215a4df066d9bfcaf5b06164aec9874d97f826b6
Walefy Lima <devbackgruposna@gmail.com>
2024-12-11 11:47:21 -0300

    feat(api): Gerando o docs personalizado da api.

--- core/api.py
+++ core/api.py
@@ -1,14 +1,29 @@
+from ninja import Swagger
 from ninja_extra import NinjaExtraAPI
 from modules.users.controllers import UsersController
 from modules.token.controller import TokenController
 from ninja_jwt.authentication import JWTAuth
+from django.contrib.admin.views.decorators import staff_member_required
 
 api = NinjaExtraAPI(
     title= "EcoVista API",
     version= "1.0.0",
     description= "Nossa API",
     app_name= "ecovista",
-    auth=JWTAuth()
+    auth=JWTAuth(),
+    docs_decorator=staff_member_required,
+    docs=Swagger(
+        settings={
+            'docExpansion': 'none',
+            'tagsSorter': 'alpha',
+            'filter': True,
+            'syntaxHighlight': {
+                'theme': 'monokai',
+                'activate': True,
+            },
+            'persistAuthorization': True,
+        }
+    )
 )
 
 api.register_controllers(UsersController)
